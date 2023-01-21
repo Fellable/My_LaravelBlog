3362004490ff22589fdadd147326dee88980b555
Fellable <crash749@yandex.ru>
2023-01-21 12:01:21 +0300

    fix: refine frontend

--- app/Http/Requests/Admin/Post/UpdateRequest.php
+++ app/Http/Requests/Admin/Post/UpdateRequest.php
@@ -29,6 +29,7 @@ class UpdateRequest extends FormRequest
             'preview_image' => 'nullable|file',
             'main_image' => 'nullable|file',
             'technology' => 'nullable|string',
+            'additional_tech' => 'nullable | string',
             'category_id' => 'required| integer |exists:categories,id',
             'tag_ids' => 'nullable|array',
             'tag_ids.*' => 'nullable | integer| exists:tags,id',
