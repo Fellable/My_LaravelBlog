2cf9216aa09eb8f7618602f518dab1fb39735e3e
Fellable <crash749@yandex.ru>
2023-02-14 23:45:28 +0300

    feature: add post filters

---
+++ app/Http/Requests/Post/FilterRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FilterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'string',
+            'content' => 'string',
+            'preview_image' => 'nullable|file',
+            'main_image' => 'nullable|file',
+            'technology' => 'nullable|string',
+            'additional_tech' => 'nullable | string',
+            'gitHub' => 'nullable | string',
+            'category_id' => ' integer |exists:categories,id',
+            'tag_ids' => 'nullable|array',
+            'tag_ids.*' => 'nullable | integer| exists:tags,id',
+        ];
+    }
+}
