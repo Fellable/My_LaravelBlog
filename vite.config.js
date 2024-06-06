eee8e122b101d305a0e448890b1ef5f4a3543015
Fellable <crash749@yandex.ru>
2024-06-06 15:42:03 +0300

    feature: add SortableJS integration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,36 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path'
+import vue from "@vitejs/plugin-vue";
+import * as fs from "fs";
+import rewrite from 'vite-plugin-rewrite'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
+    base: '',
+    server: {
+
+        hmr : {
+            host: 'laravel.test'
+        },
+
+    },
+
     plugins: [
+        vue(),
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
+        rewrite({
+            '^/@vite/client': '/@vite/client',
+        }), // Для НЕ https
+        nodePolyfills(),
     ],
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         }
     },
+
 });
