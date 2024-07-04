c65d1840d6a48f3f73b79b4e76f6f3ca8356aca6
Fellable <crash749@yandex.ru>
2024-07-04 14:10:07 +0300

    refactor: refine interface

--- app/Http/Resources/API/Post/PostShowResourse.php
+++ app/Http/Resources/API/Post/PostShowResourse.php
@@ -10,18 +10,16 @@ class PostShowResourse extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-       return [
-        'id' => $this->id,
-        'title' => $this->title,
-           'main_image' => $this->main_image,
-
-        'images' => PostResource::collection($this->postImages),
-
-    ];
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'main_image' => $this->main_image,
+            'images' => PostResource::collection($this->postImages),
+        ];
     }
 }
