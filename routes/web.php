72a925f68901f94ca1d9f6e6fc2e2ce67e5aa0e4
Fellable <crash749@yandex.ru>
2024-07-05 23:24:44 +0300

    refactor: group routes and normalize namespaces

--- routes/web.php
+++ routes/web.php
@@ -27,145 +27,112 @@ use App\Http\Controllers\Main\IndexController;
 |
 */
 
-
-
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
-    Route::get('/', IndexController::class)->name('main.index');
+    Route::get('/', 'IndexController')->name('main.index');
 });
 
-Route::prefix('admin' )->group(function (){
-    Route::group(['namespace' => 'App\Http\Controllers\Admin\Main'], function() {
+Route::prefix('admin')->group(function () {
+    Route::group(['namespace' => 'App\Http\Controllers\Admin\Main'], function () {
         Route::get('/', 'IndexController')->name('admin.main.index');
     });
-});
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin\Category', 'prefix' => 'admin/category', 'middleware'=> ['auth','admin', 'verified']], function () {
-    Route::get('/', 'IndexController')->name('admin.category.index');
-    Route::get('/create', 'CreateController')->name('admin.category.create');
-    Route::post('/categories', 'StoreController')->name('admin.category.store');
-    Route::get('/{category}', 'ShowController')->name('admin.category.show');
-    Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
-    Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
-    Route::delete('/{category}', 'DeleteController')->name('admin.category.delete');
-});
-
-
-Route::group(['namespace' => 'App\Http\Controllers\Admin\Tag', 'prefix' => 'admin/tags', 'middleware'=> ['auth','admin', 'verified']], function () {
-    Route::get('/', 'IndexController')->name('admin.tag.index');
-    Route::get('/create', 'CreateController')->name('admin.tag.create');
-    Route::post('/categories', 'StoreController')->name('admin.tag.store');
-    Route::get('/{tag}', 'ShowController')->name('admin.tag.show');
-    Route::get('/{tag}/edit', 'EditController')->name('admin.tag.edit');
-    Route::patch('/{tag}', 'UpdateController')->name('admin.tag.update');
-    Route::delete('/{tag}', 'DeleteController')->name('admin.tag.delete');
-});
-
-
-
-Route::group(['namespace' => 'App\Http\Controllers\Admin\Post', 'prefix' => 'admin/posts', 'middleware'=> ['auth','admin'] ], function () {
-    Route::get('/', 'IndexController')->name('admin.post.index');
-    Route::get('/create', 'CreateController')->name('admin.post.create');
-    Route::post('/', 'StoreController')->name('admin.post.store');
-    Route::get('/{post}', 'ShowController')->name('admin.post.show');
-    Route::get('/{post}/edit', 'EditController')->name('admin.post.edit');
-    Route::patch('/{post}', 'UpdateController')->name('admin.post.update');
-    Route::delete('/{post}', 'DeleteController')->name('admin.post.delete');
-});
-
-
-Route::group(['namespace' => 'App\Http\Controllers\Admin\User', 'prefix' => 'admin/users', 'middleware'=> ['auth','admin', 'verified']], function () {
-    Route::get('/', 'IndexController')->name('admin.user.index');
-    Route::get('/create', 'CreateController')->name('admin.user.create');
-    Route::post('/users', 'StoreController')->name('admin.user.store');
-    Route::get('/{user}', 'ShowController')->name('admin.user.show');
-    Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
-    Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
-    Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
-});
-
-
-
-
-                                                /* Личный кабинет */
-/**
- * Основной роут
- */
-Route::group(['namespace' => 'App\Http\Controllers\Lk', 'prefix' => 'lk', 'middleware'=> ['auth']], function () {
-    Route::group(['namespace' => 'Main', 'prefix'=> 'main'], function() {
-        Route::get('/', 'IndexController') -> name('lk.main.index');
+    Route::group(['prefix' => 'category', 'namespace' => 'App\Http\Controllers\Admin\Category', 'middleware' => ['auth', 'admin']], function () {
+        Route::get('/', 'IndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
+        Route::post('/', 'StoreController')->name('admin.category.store');
+        Route::get('/{category}', 'ShowController')->name('admin.category.show');
+        Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
+        Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
+        Route::delete('/{category}', 'DeleteController')->name('admin.category.delete');
     });
 
-    /**
-     * Лайки
-     */
-    Route::group(['namespace' => 'Liked', 'prefix'=> 'liked', 'middleware'=> ['auth']], function() {
-        Route::get('/', 'IndexController') ->name ('lk.liked.index');
-        Route::delete('/{post}', 'DeleteController') ->name ('lk.liked.delete');
+    Route::group(['prefix' => 'tags', 'namespace' => 'App\Http\Controllers\Admin\Tag', 'middleware' => ['auth', 'admin']], function () {
+        Route::get('/', 'IndexController')->name('admin.tag.index');
+        Route::get('/create', 'CreateController')->name('admin.tag.create');
+        Route::post('/', 'StoreController')->name('admin.tag.store');
+        Route::get('/{tag}', 'ShowController')->name('admin.tag.show');
+        Route::get('/{tag}/edit', 'EditController')->name('admin.tag.edit');
+        Route::patch('/{tag}', 'UpdateController')->name('admin.tag.update');
+        Route::delete('/{tag}', 'DeleteController')->name('admin.tag.delete');
     });
 
-    /**
-     * Комменты
-     */
-    Route::group(['namespace' => 'Comment', 'prefix'=>'comment', 'middleware'=> ['auth']], function() {
-        Route::get('/', 'IndexController') ->name ('lk.comment.index');
-        Route::get('/{comment}/edit', 'EditController') ->name ('lk.comment.edit');
-        Route::patch('/{comment}', 'UpdateController') ->name ('lk.comment.update');
-        Route::delete('/{comment}', 'DeleteController') ->name ('lk.comment.delete');
+    Route::group(['prefix' => 'posts', 'namespace' => 'App\Http\Controllers\Admin\Post', 'middleware' => ['auth', 'admin']], function () {
+        Route::get('/', 'IndexController')->name('admin.post.index');
+        Route::get('/create', 'CreateController')->name('admin.post.create');
+        Route::post('/', 'StoreController')->name('admin.post.store');
+        Route::get('/{post}', 'ShowController')->name('admin.post.show');
+        Route::get('/{post}/edit', 'EditController')->name('admin.post.edit');
+        Route::patch('/{post}', 'UpdateController')->name('admin.post.update');
+        Route::delete('/{post}', 'DeleteController')->name('admin.post.delete');
+    });
+
+    //'middleware' => ['auth', 'admin', 'verified']
+    Route::group(['prefix' => 'users', 'namespace' => 'App\Http\Controllers\Admin\User', 'middleware' => ['auth', 'admin']], function () {
+        Route::get('/', 'IndexController')->name('admin.user.index');
+        Route::get('/create', 'CreateController')->name('admin.user.create');
+        Route::post('/', 'StoreController')->name('admin.user.store');
+        Route::get('/{user}', 'ShowController')->name('admin.user.show');
+        Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
+        Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
+        Route::delete('/{user}', 'DeleteController')->name('admin.user.delete');
     });
 });
 
-/**
- * Обо мне
- */
-Route::group(['namespace' => 'App\Http\Controllers\I_am', 'prefix'=> 'about'], function() {
 
+
+// Личный кабинет
+Route::group(['namespace' => 'App\Http\Controllers\Lk', 'prefix' => 'lk', 'middleware' => ['auth']], function () {
+    Route::group(['namespace' => 'Main', 'prefix' => 'main'], function () {
+        Route::get('/', 'IndexController')->name('lk.main.index');
+    });
+
+    // Лайки
+    Route::group(['namespace' => 'Liked', 'prefix' => 'liked'], function () {
+        Route::get('/', 'IndexController')->name('lk.liked.index');
+        Route::delete('/{post}', 'DeleteController')->name('lk.liked.delete');
+    });
+
+    // Комменты
+    Route::group(['namespace' => 'Comment', 'prefix' => 'comment'], function () {
+        Route::get('/', 'IndexController')->name('lk.comment.index');
+        Route::get('/{comment}/edit', 'EditController')->name('lk.comment.edit');
+        Route::patch('/{comment}', 'UpdateController')->name('lk.comment.update');
+        Route::delete('/{comment}', 'DeleteController')->name('lk.comment.delete');
+    });
+});
+
+// Обо мне
+Route::group(['namespace' => 'App\Http\Controllers\I_am', 'prefix' => 'about'], function () {
     Route::group(['namespace' => 'About'], function () {
-            Route::get('/', 'IndexController')->name('about.index');
-        });
+        Route::get('/', 'IndexController')->name('about.index');
+    });
 
     Route::group(['namespace' => 'Find_work'], function () {
-             Route::get('/find_work', 'IndexController')->name('about.find_work');
-        });
+        Route::get('/find_work', 'IndexController')->name('about.find_work');
+    });
 });
 
-
-
-
-
-
-                                        /* Чисто для фронта */
-
-
-/**
- * Отображение постов
- */
-Route::group(['namespace' => 'App\Http\Controllers\Post', 'prefix'=> 'posts'], function () {
+// Чисто для фронта
+Route::group(['namespace' => 'App\Http\Controllers\Post', 'prefix' => 'posts'], function () {
     Route::get('/', 'IndexController')->name('post.index');
     Route::get('/{post}', 'ShowController')->name('post.show');
-    Route::group(['namespace' => 'Comment', 'prefix' => '{post}/comments'], function() {
-        // post/1/comments - NestedRoute
-        Route::post('/' , 'StoreController')->name('post.comment.store');
+
+    Route::group(['namespace' => 'Comment', 'prefix' => '{post}/comments'], function () {
+        Route::post('/', 'StoreController')->name('post.comment.store');
     });
 
-    Route::group(['namespace' => 'Like', 'prefix' => '{post}/likes'], function() {
-        // post/1/comments - NestedRoute
-        Route::post('/' , 'StoreController')->name('post.like.store');
+    Route::group(['namespace' => 'Like', 'prefix' => '{post}/likes'], function () {
+        Route::post('/', 'StoreController')->name('post.like.store');
     });
 });
 
-
-
-/**
- * Вкладка "Категории" на мейн.страничке
- */
+// Вкладка "Категории" на мейн.страничке
 Route::group(['namespace' => 'App\Http\Controllers\Category', 'prefix' => 'categories'], function () {
     Route::get('/', 'IndexController')->name('category.index');
-    Route::group(['namespace' => 'Post', 'prefix' => '{category}/posts'], function() {
-        //  NastedRoute
-        Route::get('/' , 'IndexController')->name('category.post.index');
+
+    Route::group(['namespace' => 'Post', 'prefix' => '{category}/posts'], function () {
+        Route::get('/', 'IndexController')->name('category.post.index');
     });
 });
 
-
 Auth::routes(['verify' => true]);
-
