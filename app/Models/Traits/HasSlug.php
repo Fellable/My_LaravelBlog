7652564a949db4560c1486cb4ef630971aa164f4
Fellable <crash749@yandex.ru>
2024-07-22 07:45:02 +0100

    feature: add alternative slug implementation

---
+++ app/Models/Traits/HasSlug.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Models\Traits;
+use Illuminate\Support\Str;
+
+trait HasSlug {
+
+    public function resolveRouteBindingQuery($query, $value, $filed = null)
+    {
+        $id = explode('-', $value)[0];
+        return $query->where($field ?? $this->getRouteKeyName(), $id);
+    }
+
+    public function getSlugAttribute():string
+    {
+        return Str::slug($this->id.' '.$this->title);
+    }
+}
