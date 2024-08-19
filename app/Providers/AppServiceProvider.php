0d646e46d27c5650d36f34aaed5247ead503d77a
Fellable <crash749@yandex.ru>
2024-08-19 08:59:29 +0300

    feature: add Sentry integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Sentry\Breadcrumb;
+use Sentry\Laravel\Facade as Sentry;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +31,20 @@ class AppServiceProvider extends ServiceProvider
     {
         Carbon::setLocale('ru_RU');
         Paginator::useBootstrap();
+        // Раскомментировать для Sentry
+//        DB::listen(function ($query) {
+//            Sentry::addBreadcrumb(
+//                new Breadcrumb(
+//                    Breadcrumb::LEVEL_INFO,
+//                    Breadcrumb::TYPE_DEFAULT,
+//                    'query',
+//                    $query->sql,
+//                    [
+//                        'bindings' => $query->bindings,
+//                        'time' => $query->time,
+//                    ]
+//                )
+//            );
+//        });
     }
 }
