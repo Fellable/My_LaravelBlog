44223c1418bf040f9f9a52dc7a3e745119a3e0eb
Fellable <crash749@yandex.ru>
2024-09-27 20:42:10 +0300

    feature: add default factory images and update visuals

--- app/Http/Resources/API/Post/PostShowResource.php
+++ app/Http/Resources/API/Post/PostShowResource.php
@@ -5,21 +5,15 @@ namespace App\Http\Resources\API\Post;
 use App\Http\Resources\Post\PostResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostShowResourse extends JsonResource
+class PostShowResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
             'title' => $this->title,
             'main_image' => $this->main_image,
-            'images' => PostResource::collection($this->postImages),
+            'images' => PostResource::collection($this->postImages)->resolve(),
         ];
     }
 }
